9d7f53bb7cad3f0897f657c9ee1d9e01525b4879
wangxiaoxuan <xiaoxuan.wang@v.show>
2025-01-09 11:46:01 +0800

    t push

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',  // 启用静态导出
-    basePath: '/todo-list', // 设置基础路径为你的仓库名
+    basePath: '/colorful-todo-list', // 设置基础路径为你的仓库名
     images: {
         unoptimized: true, // GitHub Pages 不支持 Next.js 的图片优化
     },
