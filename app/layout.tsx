4c268a4b50e45d9cec60a42ef49f4a830e68cf3c
wangxiaoxuan <xiaoxuan.wang@v.show>
2025-01-10 12:17:12 +0800

    big fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,7 +44,10 @@ export const metadata: Metadata = {
   viewport: {
     width: 'device-width',
     initialScale: 1,
-    maximumScale: 1,
+    minimumScale: 1,
+    maximumScale: 5,
+    userScalable: true,
+    viewportFit: 'cover',
   },
   themeColor: '#F0E6FA',
 }
